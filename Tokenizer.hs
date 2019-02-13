abb6ab7f4544ae2e60928b10fb74c6282dfd30b2
raf96 <ryabushev96@gmail.com>
2019-02-13 23:57:18 +0300

    Only with key words

--- Tokenizer.hs
+++ Tokenizer.hs
@@ -1,4 +1,10 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module Tokenizer where
+
+import Text.Parsec
+import Data.Either
+
 
 data Token = Ident String
            | KeyWord String
@@ -6,4 +12,23 @@ data Token = Ident String
            deriving (Show, Eq)
 
 tokenize :: String -> [Token]
-tokenize input = undefined
+tokenize input = fromRight [] $ parse (many getToken) "" input 
+
+getToken :: Stream s m Char => ParsecT s u m Token
+getToken = let gaps = many $ char ' ' in 
+    try (gaps *> parseKeyWord)
+
+
+parseKeyWord :: Stream s m Char => ParsecT s u m Token
+parseKeyWord = KeyWord <$> choice (try . string <$> keyWords)
+    where
+        keyWords = ["False", "await", "else", "import", "pass",
+                "None", "break", "except", "in", "raise",
+                "True", "class", "finally", "is", "return",
+                "and", "continue",  "for",       "lambda",    "try",
+                "as",         "def",        "from",       "nonlocal",   "while",
+                "assert",    "del",       "global",    "not", "with",
+                "async",      "elif",       "if",         "or",         "yield"]
+
+
+                
