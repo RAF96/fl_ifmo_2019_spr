4369bba5b461b5c6b4a29dd84cc3efdc00354f2d
Anton Riabushev <ryabushev96@gmail.com>
2019-04-16 10:48:04 +0300

    [raf96] small fix

--- Main.hs
+++ Main.hs
@@ -13,8 +13,8 @@ main = do
         let a = parseExpression input
         let r = executeExpression input
         putStrLn $ printf "Parsing %s\n" fileName
-        putStrLn $ either id show a
-        putStrLn $ either id show r
+        putStrLn $ either show show a
+        putStrLn $ either show show r
         putStrLn ""
     )
     fileNames
